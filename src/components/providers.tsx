55ceb1b2f82ab11c69dac5e541ff51613a25f33f
christian <christian.carvalho.adv@gmail.com>
2026-05-22 20:39:04 -0300

    Fix: Remoção do pisca duas vezes ao entrar no dashboard

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,4 +1,3 @@
-// src/components/providers.tsx
 "use client";
 
 import * as React from "react";
@@ -6,16 +5,6 @@ import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
     <NextThemesProvider
       attribute="class"
